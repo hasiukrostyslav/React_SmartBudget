fbba93dcd34c63bb862aa9bb9b1cac490e013e9d
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-09-27 10:19:37 +0300

    Change project configuration from Tanstack Router to React Router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,8 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteReact(), tailwindcss()],
-
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-})
+});
