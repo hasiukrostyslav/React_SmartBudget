716f92f6f08bbf8755a4c89c03508d76bfbf1c05
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-09-28 10:49:32 +0300

    Add background image to Auth Layout

--- client/src/pages/auth/AuthLayout.tsx
+++ client/src/pages/auth/AuthLayout.tsx
@@ -6,7 +6,8 @@ export default function AuthLayout() {
     <main className="flex h-screen w-screen text-slate-900 dark:text-slate-300 dark:bg-slate-900">
       <div className="relative w-7/12">
         <img
-          src="/background.jpg"
+          className="h-full"
+          src="background.jpg"
           alt="background image"
           sizes="(max-width: 768px) 50vw, (max-width: 1200px) 70vw, 100vw"
         />
