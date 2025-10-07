a33e04361cc7335b3d03b8b8f93bce0ff301c0ed
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-07 21:25:51 +0300

    Add client validation to Forgot Passowrd form

--- client/src/components/ForgotPasswordForm.tsx
+++ client/src/components/ForgotPasswordForm.tsx
@@ -1,13 +1,32 @@
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ForgotPasswordSchema } from '@/lib/schema';
 import Button from './Button';
 import Input from './Input';
 
+type FormInput = z.infer<typeof ForgotPasswordSchema>;
+
 export default function ForgotPasswordForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(ForgotPasswordSchema) });
+
+  function onSubmit(data: FormInput) {}
+
   return (
-    <form autoComplete="off" className="flex w-full flex-col gap-3">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      autoComplete="off"
+      className="flex w-full flex-col gap-3"
+    >
       <Input
         label="Email address"
-        name="email"
+        {...register('email')}
         placeholder="Please enter your email"
+        error={errors.email?.message}
       />
       <Button color="black" type="submit">
         Reset Password
