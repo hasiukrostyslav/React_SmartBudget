5f4fb42394f1c443a0e5268d76737cb551dc1039
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-20 22:55:16 +0300

    Create users Module

---
+++ server/src/modules/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Pool } from 'pg';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+@Injectable()
+export class UsersService {
+  constructor(@Inject('PG_POOL') private pool: Pool) {}
+
+  async findUserById(id: string): Promise<User | null> {
+    try {
+      const result = await this.pool.query<User>(
+        'SELECT * FROM "User" WHERE id = $1',
+        [id],
+      );
+      return result.rows[0] || null;
+    } catch (error: unknown) {
+      if (error instanceof Error) console.error('DB error:', error.message);
+      return null;
+    }
+  }
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    try {
+      const result = await this.pool.query<User>(
+        'SELECT * FROM "User" WHERE email = $1',
+        [email],
+      );
+      return result.rows[0] || null;
+    } catch (error: unknown) {
+      if (error instanceof Error) console.error('DB error:', error.message);
+      return null;
+    }
+  }
+}
