2d979340398f86a493d9214660d6fea93cf03248
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-23 21:36:04 +0300

    Configure secutity with helmet, csrf, cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { doubleCsrf } from 'csrf-csrf';
+import cookieParser from 'cookie-parser';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  const {
+    invalidCsrfTokenError,
+    generateCsrfToken,
+    validateRequest,
+    doubleCsrfProtection,
+  } = doubleCsrf({
+    getSecret: (req) => process.env.CSRF_SECRET as string,
+    getSessionIdentifier: (req) => req.user?.id || req.ip,
+  });
+
+  app.getHttpAdapter().get('/csrf-token', (req: Request, res: Response) => {
+    const csrfToken = generateCsrfToken(req, res);
+    res.json({ csrfToken });
+  });
+
+  app.use(doubleCsrfProtection);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
