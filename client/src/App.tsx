0acc80d9475be5dadba3f30106178f566d684076
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-25 13:32:14 +0300

    Add Query Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { ToastContainer } from 'react-toastify';
 import AuthLayout from './pages/auth/AuthLayout';
@@ -6,9 +8,11 @@ import SignUpPage from './pages/auth/SignUpPage';
 import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
 import NotFound from './pages/NotFound';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           <Route element={<AuthLayout />}>
@@ -23,6 +27,7 @@ export default function App() {
         </Routes>
       </BrowserRouter>
       <ToastContainer limit={1} />
-    </>
+      <ReactQueryDevtools buttonPosition="bottom-left" />
+    </QueryClientProvider>
   );
 }
