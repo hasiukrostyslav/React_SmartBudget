7bc8abcdce01f18348a8a74bbe47af75726c1023
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-01-04 16:29:57 +0200

    Add DB migration for changing user table to users

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,16 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Pool } from 'pg';
-import type { User } from './users.type';
+import type { Users } from './users.type';
 import { NewUserDto, SignUpDto } from '../auth/schemas/auth.schemas';
 
 @Injectable()
 export class UsersService {
   constructor(@Inject('PG_POOL') private pool: Pool) {}
 
-  async findUserById(id: string): Promise<User | null> {
+  async findUserById(id: string): Promise<Users | null> {
     try {
-      const result = await this.pool.query<User>(
-        'SELECT * FROM "User" WHERE id = $1;',
+      const result = await this.pool.query<Users>(
+        'SELECT * FROM "users" WHERE id = $1;',
         [id],
       );
       return result.rows[0] || null;
@@ -20,10 +20,10 @@ export class UsersService {
     }
   }
 
-  async findUserByEmail(email: string): Promise<User | null> {
+  async findUserByEmail(email: string): Promise<Users | null> {
     try {
-      const result = await this.pool.query<User>(
-        'SELECT * FROM "User" WHERE email = $1;',
+      const result = await this.pool.query<Users>(
+        'SELECT * FROM "users" WHERE email = $1;',
         [email],
       );
       return result.rows[0] || null;
@@ -38,11 +38,11 @@ export class UsersService {
     hashedPassword,
     name,
     id,
-  }: NewUserDto): Promise<User | null> {
+  }: NewUserDto): Promise<Users | null> {
     try {
       const result = await this.pool.query(
         `
-      INSERT INTO "User" (id, email, password, name)
+      INSERT INTO "users" (id, email, password, name)
       VALUES($1, $2, $3, $4)
       RETURNING *;
       `,
