69b84065acf19396864b58d016f1b3deea9ae182
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-01-19 20:33:11 +0200

    Change RedirectRoute to PublicRoute

---
+++ client/src/components/routes/PublicRoutes.tsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from 'react-router';
+import { useAuth } from '@/hooks/useAuth';
+
+export default function PublicRoutes({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const location = useLocation();
+  const { session } = useAuth();
+
+  if (session) return <Navigate to="/dashboard" replace />;
+
+  if (!session && /^\/auth\/?$/.test(location.pathname))
+    return <Navigate to="/auth/login" replace />;
+
+  return children;
+}
