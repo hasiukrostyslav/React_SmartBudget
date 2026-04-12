d988f9c544d5665d68833aa61a633c58df8070e5
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-04-12 23:36:35 +0300

    reset git tracking

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,10 +1,6 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 
-const app: Application = express();
+export const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
