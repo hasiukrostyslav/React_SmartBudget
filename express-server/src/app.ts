e4fa95520fe2cd099911f7cf51ee7e4bdbf31a30
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-04-13 15:59:06 +0300

    add helmet and cookie parser middleware

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -2,8 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, { Application, Request, Response } from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 export const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cookieParser());
