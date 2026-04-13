d23f79fdddf385878ced8cc4eb4451598593bdd0
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-04-13 13:25:33 +0300

    Finish db config

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,10 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Application, Request, Response } from 'express';
 
 export const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
-});
