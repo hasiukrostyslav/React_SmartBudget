f0062fa297a8092ac3057fbb2897860059902f71
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-04-13 12:23:44 +0300

    add db folder

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,6 +1,10 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 export const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
