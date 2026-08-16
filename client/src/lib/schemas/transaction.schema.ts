f0dbd518e4019a40f06ebb1bded1b4c619afbbb7
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-08-16 12:28:45 +0300

    update transaction schema

--- client/src/lib/schemas/transaction.schema.ts
+++ client/src/lib/schemas/transaction.schema.ts
@@ -24,7 +24,11 @@ export const TransactionSchema = z.object({
   amount: z.coerce
     .number()
     .positive({ message: 'Amount must be a positive number.' }),
-  description: z.string().optional(),
+  description: z
+    .string()
+    .nullish()
+    .transform((v) => v?.trim() || null)
+    .optional(),
   status: z.enum(STATUSES).default('COMPLETED'),
   createdAt: z.date(),
 });
@@ -45,4 +49,22 @@ export const SearchParamsSchema = z.object({
     .default('date'),
   order: z.enum(['asc', 'desc']).optional().default('desc'),
   search: z.string().optional().default(''),
+  category: z.string().optional().default(''),
+  account: z.string().optional().default(''),
+  date: z.string().optional().default(''),
+  amount: z.string().optional().default(''),
+  currency: z.string().optional().default(''),
+  status: z.string().optional().default(''),
+  type: z.string().optional().default(''),
+});
+
+export const FilterParamsSchema = SearchParamsSchema.pick({
+  search: true,
+  category: true,
+  account: true,
+  date: true,
+  amount: true,
+  currency: true,
+  status: true,
+  type: true,
 });
